debaf5e3b150cf17f619da23f3f0873a9f2a46cf
Rohan <rohandasd0@gmail.com>
2021-05-01 11:05:02 +0530

    added href on tweet button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import axios from "axios";
 
-function App() {
+
+export default function App() {
+  const [Quotes, setQuote] = useState("");
+
+  let cancelToken;
+
+  if (typeof cancelToken != typeof undefined) {
+    cancelToken.cancel("Cancelling the previos requests");
+  }
+
+  cancelToken = axios.CancelToken.source();
+
+  const getQuote = async () => {
+    const { data } = await axios.get(`https://api.quotable.io/random`, {
+      cancelToken: cancelToken.token,
+    });
+    setQuote(data);
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="quote-box">
+        <div class="quote-text">
+          <i class="fa fa-quote-left">{Quotes.content}</i>
+          <span id="text"></span>
+        </div>
+        <div class="quote-author">
+          {Quotes.author}
+          <span id="author"></span>
+        </div>
+        <div class="buttons">
+          <a
+            href="twitter.com/intent/tweet"
+            class="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+          >
+            <i class="fa fa-twitter"></i>
+          </a>
+
+          <button class="button" id="new-quote" onClick={getQuote}>
+            New quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
